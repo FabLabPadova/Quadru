af7400d6a8e0f23a5b1e676e771f86789d9b929e
keyro90 <andreatgiusti@gmail.com>
2016-04-07 13:12:15 +0200

    unsigned added.

--- arduino_sketch/RX_stuff/utility.cpp
+++ arduino_sketch/RX_stuff/utility.cpp
@@ -5,7 +5,7 @@
   IV: len int shex's length.
   OR: shex converted into integer number.
 */
-int conv_hex_to_dec(const char * shex, const unsigned int len) {
+unsigned int conv_hex_to_dec(const char * shex, const unsigned int len) {
   unsigned int dec = 0;
   for (unsigned int i = 0; i < len; i++)
     dec += (_pow(16, len - i - 1)) * ( (shex[i] >= '0' && shex[i] <= '9') * (shex[i] - '0') + (shex[i] >= 'A' && shex[i] <= 'F') * (shex[i] - 'A' + 10) );
@@ -13,7 +13,7 @@ int conv_hex_to_dec(const char * shex, const unsigned int len) {
 }//conv_hex_to_dec
 
 //pow function for integer. So, the original one return result minus 1...?
-int _pow (const unsigned int base, const unsigned int exponent) {
+unsigned int _pow (const unsigned int base, const unsigned int exponent) {
   unsigned int ris = 1;
   for (unsigned int i = 0; i < exponent; i++)
     ris *= base;
--- arduino_sketch/RX_stuff/utility.h
+++ arduino_sketch/RX_stuff/utility.h
@@ -1,7 +1,7 @@
 #ifndef UTILITY_H   // if x.h hasn't been included yet...
 #define UTILITY_H
 
-int conv_hex_to_dec(const char * shex, const unsigned int len);
-int _pow (const unsigned int base, const unsigned int exponent);
+unsigned int conv_hex_to_dec(const char * shex, const unsigned int len);
+unsigned int _pow (const unsigned int base, const unsigned int exponent);
 
 #endif 
