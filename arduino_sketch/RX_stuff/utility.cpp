a75c14eeb03eb501f161a522aa1d5d4c853da280
Andrea Tazio Giusti <andreatgiusti@gmail.com>
2016-04-07 18:40:03 +0200

    Update utility.cpp

--- arduino_sketch/RX_stuff/utility.cpp
+++ arduino_sketch/RX_stuff/utility.cpp
@@ -5,11 +5,11 @@
   IV: len int shex's length.
   OR: shex converted into integer number.
 */
-unsigned int conv_hex_to_dec(const char * shex, const unsigned int len) {
-  unsigned int dec = 0;
-  for (unsigned int i = 0; i < len; i++)
-    dec += (_pow(16, len - i - 1)) * ( (shex[i] >= '0' && shex[i] <= '9') * (shex[i] - '0') + (shex[i] >= 'A' && shex[i] <= 'F') * (shex[i] - 'A' + 10) );
-  return dec;
+unsigned int conv_hex_to_dec(const char *shex, const unsigned int len){
+  unsigned int res = 0;
+  for (unsigned int i = 0; i<len; i++)
+    res = res << 4 | ((shex[i] >= '0' && shex[i] <= '9') ? (shex[i] - '0') : (shex[i] - 'A' + 10));
+  return res;
 }//conv_hex_to_dec
 
 //pow function for integer. So, the original one return result minus 1...?
