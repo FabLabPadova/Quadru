0e68168d3b7eb7a3c6a718ba757f818130227def
Andrea Tazio Giusti <andreatgiusti@gmail.com>
2016-04-07 13:03:31 +0200

    Update utility.h

--- arduino_sketch/RX_stuff/utility.h
+++ arduino_sketch/RX_stuff/utility.h
@@ -1,7 +1,7 @@
 #ifndef UTILITY_H   // if x.h hasn't been included yet...
 #define UTILITY_H
 
-int conv_hex_to_dec(const char * shex, const unsigned int len);
-int _pow (unsigned int base, unsigned int exponent);
+inline int conv_hex_to_dec(const char * shex, const unsigned int len);
+inline int _pow (unsigned int base, unsigned int exponent);
 
 #endif 
