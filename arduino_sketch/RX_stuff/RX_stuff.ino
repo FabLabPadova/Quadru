58528e935e5e16bae2f4d9a1922a037d2e1e5e47
Andrea Tazio Giusti <andreatgiusti@gmail.com>
2016-04-10 19:42:14 +0200

    Update RX_stuff.ino

--- arduino_sketch/RX_stuff/RX_stuff.ino
+++ arduino_sketch/RX_stuff/RX_stuff.ino
@@ -6,7 +6,6 @@
 #define CE_PIN   9
 #define CSN_PIN 10
 #define LED_ON 7
-#define LED_OFF 8
 #define LED_ALERT 4
 #define PIPE 0xE8E8F0F0E1LL
 #define BUFFER_SIZE 32
@@ -14,7 +13,7 @@
 
 char stuff[BUFFER_SIZE];
 bool full_string_complete = false, end_str = false;
-int current_line = 0, previous_line = 4;
+int current_line = 0, previous_line = 4, timeFailure = 0;
 String rec = "";
 RF24 radio(CE_PIN, CSN_PIN);
 
@@ -23,43 +22,33 @@ void scan_str();
 
 void setup() {
   Serial.begin(9600);
-  pinMode(LED_ON, OUTPUT);
-  pinMode(LED_OFF, OUTPUT);
   pinMode(LED_ALERT, OUTPUT);
-  digitalWrite(LED_OFF, HIGH);
   radio.begin();
   radio.openReadingPipe(1, PIPE);
   radio.setChannel(125);
   radio.setPALevel(RF24_PA_HIGH);
   radio.setDataRate(RF24_2MBPS);
   radio.startListening();
-  memset(stuff, 0, BUFFER_SIZE);
 }//setup
 
 void loop() {
   if (radio.available()) {
-    while (!radio.read( stuff, BUFFER_SIZE )){}//wait transmission done and receive data.
-    unsigned int i = 0;
-    while (i < BUFFER_SIZE && !(end_str = stuff[i] == '!'))
-      rec += stuff[i++];
-    memset(stuff, 0, BUFFER_SIZE);
+    char * stuff = new char();
+    while (!radio.read( stuff, sizeof(*stuff))){}//wait transmission done and receive data.
+    Serial.print(*stuff);
+    if (!(end_str = (*stuff) == '!'))
+      rec += (*stuff);
+    delete stuff;
   }//if-available
   if (end_str) {
-    activityLed();
     current_line = rec.charAt(0) - '0';
     digitalWrite(LED_ALERT, !((current_line == 0 && previous_line == 4) || (current_line == previous_line+1))); //are you sure to be brave enough? :)
     previous_line = current_line;
     scan_str();
     rec = "";
     end_str = false;
-    activityLed(false);
   }//if
 }//loop
-
-void activityLed (bool in_activity){
-  digitalWrite(LED_OFF, !in_activity);   
-  digitalWrite(LED_ON, in_activity);
-}//activityLed
 
 //Scan rec string. Every 3 character function captures string and convert hex to dec.
 void scan_str() {
@@ -77,5 +66,3 @@ void scan_str() {
   }//for
   Serial.println();
 }//scan_str
-
-
