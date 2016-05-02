89c57c1f121b032db19803b7cd7bbef6ff8f71b8
Andrea Tazio Giusti <andreatgiusti@gmail.com>
2016-05-02 14:19:26 +0200

    servo managed

--- arduino_sketch/arduinoleg/arduinoleg.ino
+++ arduino_sketch/arduinoleg/arduinoleg.ino
@@ -1,12 +1,21 @@
+#include <Servo.h>
 #include <Wire.h>
+
 #define N_SLAVE 0
+#define N_PART 3
+
+const int pin_servo[N_PART] = {3,5,6};
 int rx_wire;
+int servo_to_act = 0;
+Servo motor_leg[N_PART];
 
 void setup() {
-  Wire.begin();
   Serial.begin(9600);
+  for (int i=0; i<N_PART; i++)
+    motor_leg[i].attach(pin_servo[i]);
+  Wire.begin();
   Wire.onReceive(receiveEvent);
-}
+}//setup
 
 void loop() {}
 
@@ -14,6 +23,8 @@ void receiveEvent (int howMany){
   while (Wire.available()){
     rx_wire = Wire.read() << 8;
     Serial.println(rx_wire);
-  }
+    motor_leg[servo_to_act].writeMicroseconds(rx_wire);
+    servo_to_act = (servo_to_act == N_PART-1) ? (0) : (servo_to_act+1);
+  }//receiveEvent
 }//receiveEvent
 
