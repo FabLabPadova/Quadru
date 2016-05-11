e01cc8ceb8fb6fba3479ecba72a71dec229bc339
Andrea Tazio Giusti <andreatgiusti@gmail.com>
2016-05-11 10:33:46 +0200

    delete useless directory.

--- arduino_sketch/Tester_servo/Tester_servo.ino
+++ arduino_sketch/Tester_servo/Tester_servo.ino
@@ -4,6 +4,7 @@
 #define BUTTON_2    3  // pulsante rotazione oraria gradi 
 #define BUTTON_3    4  // pulsante rotazione antioraria con microsecondi
 #define BUTTON_4    5  // pulsante rotazione oraria con microsecondi
+#define BUTTON_5    7  // pulsante rotazione a step
 #define GRADI_0 90
 #define GRADI_1 0
 #define GRADI_2 180
@@ -19,6 +20,7 @@ void setup () {
   pinMode(BUTTON_2 , INPUT);
   pinMode(BUTTON_3 , INPUT);
   pinMode(BUTTON_4 , INPUT);
+  pinMode(BUTTON_5 , INPUT);
   Servo1.attach(6, 700, 2350);      // Servo1 e' collegato al pin 6 , limiti minimi e massimi della durata degli impulsi ,
                                     // questo per non forzare il servo nelle due posizioni di finecorsa, si riduce il movimento
                                     // angolare
@@ -39,6 +41,26 @@ void loop () {
     Servo1.writeMicroseconds (MICROSEC1);
   else if (digitalRead(BUTTON_4) == HIGH)
     Servo1.writeMicroseconds (MICROSEC2);
+  else if (digitalRead(BUTTON_5) == HIGH){          // il servo si muove a step di un grado con la variabile passo
+    for(int passo = 0;passo < 180; passo += 1)      // da 0 a 180 , il valore verra' visualizzato sulla seriale
+    {
+      Servo1.write(passo);
+    delay(20);
+    angle = Servo1.read();
+    Serial.print("Angle position");
+    Serial.println(angle); 
+    }
+    delay(500);
+    for(int passo = 180; passo >= 1; passo -= 1)
+    {
+      Servo1.write(passo);
+      angle = Servo1.read();
+    Serial.print("Angle position");
+    Serial.println(angle);
+      delay(20);
+    }
+  delay(200);
+  }
 
 
   /*  Serial.print("BUTTON_1");
